0d63978b4f2a0707fccf8d1b6a16fde9589e9fda
Rafal <rafalgad@wp.pl>
2024-05-17 19:51:21 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import company.abstracts.Employee;
 import company.models.Manager;
 import company.models.Worker;
@@ -6,19 +8,23 @@ import company.models.Worker;
 public class Main {
     public static void main(String[] args) {
         
-        Employee[] employees = {
-            new Worker ("Worker1", 3000,1),
-            new Worker("Worker2", 5000 , 1),
-            new Worker("Worker 3", 8000, 3),
-            new Worker("worker 4", 4500, 4),
-            new Manager("Manager", 10000, 5)
-         };
+        ArrayList<Employee> employees = new ArrayList<>();
 
-         for(Employee employee: employees) {
-            System.out.println("Name: " + employee.getName() + ", Salary: " + employee.getSalary()) ;
-            employee.work();
-         }
+            employees.add(new Worker ("Worker1", 3000, 0, "05-12-2020", "Developer"));
+            employees.add(new Worker("Worker2", 5000 , 1,"05-12-2020", "Developer"));
+            employees.add(new Worker("Worker3", 8000, 3,"04-09-2020", "Developer"));
+            employees.add(new Worker("worker4", 4500, 4,"05-11-2021", "Tester"));
+            employees.add(new Manager("Manager", 10000, 5,"05-11-2010", "Project Manager"));
 
+            
+            
+            
+            for (Employee employee : employees) {
+                System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() +
+                                   ", Position: " + employee.getPosition() +
+                                   ", Hire date: " + employee.getHireDate() +
+                                   ", Salary:" + employee.getSalary() + ")");
+            }
          
 
 
